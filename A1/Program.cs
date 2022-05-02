8b1ef4cafd28e861bd26c12c7407cebd4edfc829
LinusTM <9331149-LinusTD@users.noreply.gitlab.com>
2022-05-02 19:19:47 +0200

    initial commit

---
+++ A1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace A1 {
+    class Program {
+        static int sum = 0;
+        static object _lock = new object();
+
+        static void Main(string[] args) {
+            Thread[] threads = new Thread[10];
+
+            for(int i = 0; i < 10; i+=2) {
+                threads[i] = new Thread(AddOne);
+                threads[i+1] = new Thread(SubTwo);
+            }
+
+            for(int i = 0; i < 10; i++) {
+                threads[i].Start();
+            }
+
+            for(int i = 0; i < 10; i++) {
+                threads[i].Join();
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        public static void AddOne() {
+            lock(_lock) { 
+                sum+=2; 
+            }
+        }
+
+        public static void SubTwo() {
+            lock(_lock) {
+                sum--;
+            }
+        }
+    }
+}
---
+++ A2-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace A2_3 {
+    class Program {
+        static int sum = 0;
+        static object _lock = new object();
+        
+        static void Main(string[] args) {
+            Thread printPound = new Thread(PrintPound);
+            Thread printAs = new Thread(PrintAs);
+            
+            printPound.Start();
+            printAs.Start();
+        }
+
+        public static void PrintPound() {
+            while(true) {
+                lock (_lock) {
+                    for(int i = 0; i < 60; i++) {
+                        Console.Write("#");
+                    }
+                    Console.Write($" {Add()} \n");
+                }
+            }
+        }
+        
+        public static void PrintAs() {
+            while(true) {
+                lock (_lock) {
+                    for(int i = 0; i < 60; i++) {
+                        Console.Write("*");
+                    }
+                    Console.Write($" {Add()}\n");
+                }
+            }
+        }
+        
+        public static int Add() {
+            return sum+=60; 
+        }
+    }
+}
